0b5fd8a78ee9ef22d8ec5388f3fd0f822697c971
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2023-11-10 00:17:38 -0500

    Add docgen capability

--- lakefile.lean
+++ lakefile.lean
@@ -31,3 +31,6 @@ lean_lib «IMOSLLean4» where
   moreLeanArgs := moreLeanArgs
   weakLeanArgs := weakLeanArgs
   -- add any library configuration options here
+
+meta if get_config? env = some "dev" then -- dev is so not everyone has to build it
+require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
