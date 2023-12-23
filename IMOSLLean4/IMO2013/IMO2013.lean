b5f392f070fd00a811abc2202bcf197eb37a0a38
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2023-12-23 13:41:53 -0500

    2013 N2: Formalize

--- IMOSLLean4/IMO2013/IMO2013.lean
+++ IMOSLLean4/IMO2013/IMO2013.lean
@@ -1,3 +1,4 @@
 -- Root for the `IMO2013` problems.
 import IMOSLLean4.IMO2013.A5.A5
 import IMOSLLean4.IMO2013.N1.N1
+import IMOSLLean4.IMO2013.N2.N2
---
+++ IMOSLLean4/IMO2013/N2/N2.lean
@@ -0,0 +1,66 @@
+/-
+Copyright (c) 2023 Gian Cordana Sanjaya. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Gian Cordana Sanjaya
+-/
+
+import Mathlib.Algebra.BigOperators.Multiset.Basic
+import Mathlib.Data.Nat.Parity
+
+/-!
+# IMO 2013 N2 (P1)
+
+Prove that, for any $k, n \in ℕ$ with $n > 0$,
+  there exists positive integers $m_1, m_2, \ldots, m_k$ such that
+$$ 1 + \frac{2^k - 1}{n} = \prod_{i = 1}^k \left(1 + \frac{1}{m_i}\right). $$
+-/
+
+namespace IMOSL
+namespace IMO2013N2
+
+open Multiset
+
+/-- A more general predicate: the goal is to prove `good k (2^k - 1)` -/
+abbrev good (k c : ℕ) := ∀ n : ℕ, 0 < n →
+  ∃ S : Multiset ℕ, card S = k ∧ (∀ m ∈ S, 0 < m) ∧
+    ((n + c : ℕ) : ℚ) / n = (S.map λ (m : ℕ) ↦ ((m + 1 : ℕ) : ℚ) / m).prod
+
+
+
+/-- General form of induction step -/
+theorem good_two_mul_add_one (h : good k c) : good (k + 1) (2 * c + 1) := by
+  intro n h0
+  rcases n.even_or_odd' with ⟨t, rfl | rfl⟩
+  -- Case 1: `n = 2t`
+  · replace h0 := pos_of_mul_pos_right h0 (Nat.zero_le 2)
+    rcases h t h0 with ⟨T, rfl, h1, h2⟩
+    have X := t.add_pos_left h0 c
+    refine ⟨(2 * (t + c)) ::ₘ T, card_cons _ T,
+      forall_mem_cons.mpr ⟨mul_pos (Nat.succ_pos 1) X, h1⟩, ?_⟩
+    rw [map_cons, prod_cons, ← h2, ← add_assoc, ← mul_add,
+      div_mul_div_comm, Nat.cast_mul, Nat.cast_mul, mul_right_comm]
+    exact (mul_div_mul_right _ _ <| Nat.cast_ne_zero.mpr X.ne.symm).symm
+  -- Case 2: `n = 2t + 1`
+  · have X := t.succ_pos
+    rcases h (t + 1) X with ⟨T, rfl, h1, h2⟩
+    refine ⟨(2 * t + 1) ::ₘ T, card_cons _ T,
+      forall_mem_cons.mpr ⟨(2 * t).succ_pos, h1⟩, ?_⟩
+    rw [map_cons, prod_cons, ← h2, add_add_add_comm, add_right_comm,
+      add_assoc (2 * t) 1, ← mul_add_one, ← mul_add, div_mul_div_comm,
+      Nat.cast_mul, Nat.cast_mul, mul_right_comm]
+    exact (mul_div_mul_right _ _ <| Nat.cast_ne_zero.mpr X.ne.symm).symm
+
+
+
+
+
+/-- Final solution -/
+theorem final_solution : ∀ k : ℕ, good k (2 ^ k - 1)
+  | 0 => λ n h ↦ ⟨0, rfl, λ m h ↦ absurd h (not_mem_zero m), by
+      rw [pow_zero, Nat.sub_self, add_zero]
+      exact div_self (Nat.cast_ne_zero.mpr h.ne.symm)⟩
+  | k + 1 => by
+      have h := good_two_mul_add_one (final_solution k)
+      have h0 := k.one_le_pow 2 (Nat.succ_pos 1)
+      rwa [two_mul, add_assoc, Nat.sub_add_cancel h0, add_comm _ (2 ^ k),
+        ← Nat.add_sub_assoc h0, ← two_mul, ← pow_succ] at h
