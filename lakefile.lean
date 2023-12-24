cd9da68f798fe2e03a8af5f2610e8641a5e35e92
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2023-12-24 01:59:33 -0500

    Update mathlib4

--- lakefile.lean
+++ lakefile.lean
@@ -1,35 +1,14 @@
 import Lake
 open Lake DSL
 
-def moreServerArgs := #[
-  "-Dpp.unicode.fun=true", -- pretty-prints `fun a ↦ b`
-  "-Dpp.proofs.withType=false"
-]
-
--- These settings only apply during `lake build`, but not in VSCode editor.
-def moreLeanArgs := moreServerArgs
-
--- These are additional settings which do not affect the lake hash,
--- so they can be enabled in CI and disabled locally or vice versa.
--- Warning: Do not put any options here that actually change the olean files,
--- or inconsistent behavior may result
-def weakLeanArgs : Array String :=
-  if get_config? CI |>.isSome then
-    #["-DwarningAsError=true"]
-  else
-    #[]
-
 package «IMOSLLean4» where
-  moreServerArgs := moreServerArgs
-  -- add any package configuration options here
+  leanOptions := #[⟨`pp.unicode.fun, true⟩]
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
 
 @[default_target]
 lean_lib «IMOSLLean4» where
-  moreLeanArgs := moreLeanArgs
-  weakLeanArgs := weakLeanArgs
   -- add any library configuration options here
 
 meta if get_config? env = some "dev" then -- dev is so not everyone has to build it
