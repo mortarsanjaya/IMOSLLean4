37d76cee8cd052150db0d07f96215db80da89243
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2024-01-02 22:42:34 -0500

    2020 N5: "Even" monoid homomorphism

---
+++ IMOSLLean4/IMO2020/N5/ClassPrime/EvenMonoidHom/Basic.lean
@@ -0,0 +1,96 @@
+/-
+Copyright (c) 2024 Gian Cordana Sanjaya. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Gian Cordana Sanjaya
+-/
+
+import Mathlib.Algebra.Ring.Defs
+import Mathlib.Algebra.Group.Hom.Basic
+
+/-!
+# "Even" homomorphism `M →* N`
+
+Let `M` and `N` be a monoid, with `M` containing `-1`.
+That is, `M` has a negation operator `- : M → M` such that
+  `-(-x) = x` and `(-x)y = x(-y) = -(xy)` for all `x y : M`.
+An even homomorphism `f : M →* N` is a homomorphism with `f(-1) = 1`.
+If `M` is a commutative group, then the set of such homomorphism is
+  isomorphic with `M⧸{±1} →* N`, but is easier to work with than `M⧸{±1} →* N`.
+The isomorphism will be provided in `QuotIso.lean`.
+-/
+
+namespace IMOSL
+namespace IMO2020N5
+
+structure EvenMonoidHom (M : Type*) [MulOneClass M] [HasDistribNeg M]
+    (N : Type*) [MulOneClass N] extends MonoidHom M N where
+  protected map_neg_one' : toMonoidHom.toFun (-1) = 1
+
+
+
+
+
+namespace EvenMonoidHom
+
+/-! #### Basic properties -/
+section MulOneClass
+
+variable [MulOneClass M] [HasDistribNeg M] [MulOneClass N]
+
+instance : CoeFun (EvenMonoidHom M N) (λ _ ↦ M → N) :=
+  ⟨λ f ↦ f.toMonoidHom.toFun⟩
+
+@[simp] lemma map_one (f : EvenMonoidHom M N) :
+    f 1 = 1 := f.map_one'
+
+@[simp] lemma map_mul (f : EvenMonoidHom M N) (x y) :
+    f (x * y) = f x * f y := f.map_mul' x y
+
+@[simp] lemma map_neg_one (f : EvenMonoidHom M N) :
+    f (-1) = 1 := f.map_neg_one'
+
+instance funLike (M : outParam Type*) [MulOneClass M] [HasDistribNeg M]
+    (N : outParam Type*) [MulOneClass N] :
+    FunLike (EvenMonoidHom M N) M (λ _ ↦ N) where
+  coe f := f.toMonoidHom.toFun
+  coe_injective' _ _ h := by rw [mk.injEq]; exact MonoidHom.ext (congrFun h)
+
+theorem coe_inj {f g : EvenMonoidHom M N} : (f : M → N) = g ↔ f = g :=
+  ⟨λ h ↦ FunLike.coe_injective' h, λ h ↦ h ▸ rfl⟩
+
+@[ext] theorem ext {f g : EvenMonoidHom M N} (h : ∀ x, f x = g x) : f = g :=
+  FunLike.ext _ _ h
+
+theorem ext_iff {f g : EvenMonoidHom M N} : f = g ↔ ∀ x, f x = g x :=
+  ⟨λ h _ ↦ h ▸ rfl, ext⟩
+
+
+instance : One (EvenMonoidHom M N) where one :=
+  { toMonoidHom := 1
+    map_neg_one' := rfl }
+
+@[simp] theorem one_apply : (1 : EvenMonoidHom M N) x = 1 := rfl
+
+end MulOneClass
+
+
+
+/-! #### Commutative monoid structure -/
+section CommMonoid
+
+variable [CommMonoid M] [HasDistribNeg M] [CommMonoid N]
+
+instance : Mul (EvenMonoidHom M N) where mul := λ f g ↦
+  { toMonoidHom := f.toMonoidHom * g.toMonoidHom
+    map_neg_one' := one_mul (1 : N) ▸ congr_arg₂ _ f.map_neg_one g.map_neg_one }
+
+@[simp] theorem mul_apply {f g : EvenMonoidHom M N} :
+    (f * g) x = f x * g x := rfl
+
+instance : CommMonoid (EvenMonoidHom M N) :=
+  { mul_comm := λ _ _ ↦ ext λ _ ↦ mul_comm _ _
+    mul_assoc := λ _ _ _ ↦ ext λ _ ↦ mul_assoc _ _ _
+    one_mul := λ _ ↦ ext λ _ ↦ one_mul _
+    mul_one := λ _ ↦ ext λ _ ↦ mul_one _ }
+
+end CommMonoid
---
+++ IMOSLLean4/IMO2020/N5/ClassPrime/EvenMonoidHom/QuotIso.lean
@@ -0,0 +1,73 @@
+/-
+Copyright (c) 2024 Gian Cordana Sanjaya. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Gian Cordana Sanjaya
+-/
+
+import IMOSLLean4.IMO2020.N5.ClassPrime.EvenMonoidHom.Basic
+import Mathlib.GroupTheory.QuotientGroup
+
+/-!
+# Isomorphism between `EvenMonoidHom M N` and `M/{±1} →* N`
+
+This file provides an isomorphism between `EvenMonoidHom M N` and `M/{±1} →* N`.
+It is made as explicit as possible.
+-/
+
+namespace IMOSL
+namespace IMO2020N5
+namespace EvenMonoidHom
+
+variable (M N : Type*) [CommGroup M] [HasDistribNeg M] [CommMonoid N]
+
+/-- The function from `EvenMonoidHom M N` to `M/{±1} →* N` -/
+def toQuotHom' (f : EvenMonoidHom M N) : M ⧸ Subgroup.zpowers (-1) →* N :=
+  QuotientGroup.lift _ f.toMonoidHom (Subgroup.zpowers_le_of_mem f.map_neg_one)
+
+lemma toQuotHom'_one : toQuotHom' M N 1 = 1 :=
+  MonoidHom.ext λ x ↦ x.induction_on λ _ ↦ rfl
+
+lemma toQuotHom'_mul (f g : EvenMonoidHom M N) :
+    toQuotHom' M N (f * g) = toQuotHom' M N f * toQuotHom' M N g :=
+  MonoidHom.ext λ x ↦ x.induction_on λ _ ↦ rfl
+
+/-- `toQuotHom'` as a homomorphism -/
+def toQuotHom : EvenMonoidHom M N →* (M ⧸ Subgroup.zpowers (-1) →* N) :=
+  { toFun := toQuotHom' M N
+    map_one' := toQuotHom'_one M N
+    map_mul' := toQuotHom'_mul M N }
+
+
+
+/-- The function from `(M/{±1} →* N)` to `EvenMonoidHom M N` -/
+def ofQuotHom' (φ : M ⧸ Subgroup.zpowers (-1) →* N) : EvenMonoidHom M N :=
+  { toMonoidHom := φ.comp (QuotientGroup.mk' (Subgroup.zpowers (-1 : M)))
+    map_neg_one' := φ.map_one ▸ congr_arg φ.toFun
+      ((QuotientGroup.eq_one_iff _).mpr (Subgroup.mem_zpowers _)) }
+
+lemma ofQuotHom'_one : ofQuotHom' M N 1 = 1 := rfl
+
+lemma ofQuotHom'_mul (f g : M ⧸ Subgroup.zpowers (-1) →* N) :
+    ofQuotHom' M N (f * g) = ofQuotHom' M N f * ofQuotHom' M N g := rfl
+
+/-- `ofQuotHom` as a homomorphism -/
+def ofQuotHom : (M ⧸ Subgroup.zpowers (-1) →* N) →* EvenMonoidHom M N :=
+  { toFun := ofQuotHom' M N
+    map_one' := ofQuotHom'_one M N
+    map_mul' := ofQuotHom'_mul M N }
+
+
+
+lemma ofQuotHom'_toQuotHom' (f : EvenMonoidHom M N) :
+    ofQuotHom' M N (toQuotHom' M N f) = f := rfl
+
+lemma toQuotHom'_ofQuotHom' (φ : M ⧸ Subgroup.zpowers (-1) →* N) :
+    toQuotHom' M N (ofQuotHom' M N φ) = φ := by
+  ext x; rfl
+
+def equivQuotHom : EvenMonoidHom M N ≃* (M ⧸ Subgroup.zpowers (-1) →* N) :=
+  { toFun := toQuotHom' M N
+    invFun := ofQuotHom' M N
+    left_inv := ofQuotHom'_toQuotHom' M N
+    right_inv := toQuotHom'_ofQuotHom' M N
+    map_mul' := toQuotHom'_mul M N }
