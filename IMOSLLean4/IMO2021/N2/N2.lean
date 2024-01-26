c46cd44b04c8fdb501ed439126fb31d2b98ee8fa
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2024-01-26 01:07:11 -0500

    2021 N2: Optimize + refactor

--- IMOSLLean4/IMO2021/N2/N2.lean
+++ IMOSLLean4/IMO2021/N2/N2.lean
@@ -4,8 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Gian Cordana Sanjaya
 -/
 
-import Mathlib.Algebra.GroupPower.CovariantClass
-import Mathlib.Tactic.Ring
+import Mathlib.Data.Nat.Sqrt
 
 /-!
 # IMO 2021 N2 (P1)
@@ -33,42 +32,68 @@ theorem good_cond1 (h : n ≤ a) (h0 : a < b) (h1 : b < c) (h2 : c ≤ 2 * n)
 
 theorem good_cond2 (h : n ≤ 2 * k ^ 2 + 4 * k) (h0 : k ^ 2 + 6 * k + 8 ≤ n) :
     good n := by
-  refine good_cond1 (b := 2 * k ^ 2 + 8 * k + 9) (k := 2 * k + 3) (l := 2 * k + 4)
-    (m := 2 * k + 5) h ?_ ?_ (Nat.mul_le_mul_left 2 h0) ?_ ?_ ?_
+  refine good_cond1 (b := 2 * k ^ 2 + 8 * k + 9) (k := 2 * k + 3)
+    (l := 2 * k + 4) (m := 2 * k + 5) h ?_ ?_
+    (Nat.mul_le_mul_left 2 h0) ?_ ?_ ?_
   -- `2k^2 + 4k < 2k^2 + 8k + 9`
-  · rw [Nat.lt_succ_iff, le_iff_exists_add]
-    exact ⟨4 * k + 8, by ring⟩
+  · rw [Nat.add_assoc, Nat.add_lt_add_iff_left, Nat.lt_succ_iff]
+    refine (Nat.mul_le_mul_right k ?_).trans (Nat.le_add_right _ _)
+    exact Nat.le_mul_of_pos_left 4 (Nat.succ_pos 1)
   -- `2k^2 + 8k + 9 < 2(k^2 + 6k + 8)`
-  · rw [← Nat.succ_le_iff, Nat.succ_eq_add_one, le_iff_exists_add]
-    exact ⟨4 * k + 6, by ring⟩
-  -- The three equalities
-  all_goals ring
+  · rw [Nat.mul_add, Nat.mul_add, ← Nat.mul_assoc,
+      Nat.lt_succ_iff, Nat.add_assoc, Nat.add_assoc,
+      Nat.add_le_add_iff_left, Nat.add_le_add_iff_right]
+    refine (Nat.mul_le_mul_right k ?_).trans (Nat.le_add_right _ _)
+    exact Nat.mul_le_mul_left 4 (Nat.le_succ 2)
+  -- `2k^2 + 4k + (2k^2 + 8k + 9) = (2k + 3)^2`
+  · rw [← Nat.add_assoc, Nat.add_add_add_comm, ← Nat.add_mul, ← Nat.add_mul,
+      add_sq, Nat.mul_pow, Nat.mul_right_comm, ← Nat.mul_assoc]
+    rfl
+  -- `2k^2 + 4k + 2(k^2 + 6k + 8) = (2k + 4)^2`
+  · rw [Nat.mul_add, ← Nat.add_assoc, Nat.mul_add, Nat.add_add_add_comm,
+      ← Nat.add_mul, ← Nat.mul_assoc, ← Nat.add_mul, add_sq, Nat.mul_pow,
+      Nat.mul_right_comm, ← Nat.mul_assoc]
+    rfl
+  -- `2k^2 + 8k + 9 + 2(k^2 + 6k + 8) = (2k + 5)^2`
+  · rw [Nat.mul_add, Nat.add_add_add_comm, Nat.mul_add, ← Nat.mul_assoc,
+      Nat.add_add_add_comm (2 * k ^ 2), ← Nat.add_mul, ← Nat.add_mul,
+      add_sq, Nat.mul_pow, Nat.mul_right_comm, ← Nat.mul_assoc]
+    rfl
+
+section
+
+set_option trace.profiler true
+set_option trace.profiler.threshold 200
+
+theorem good_cond2_k_witness :
+    ∀ n, 99 ≤ n → ∃ k, n ≤ 2 * k ^ 2 + 4 * k ∧ k ^ 2 + 6 * k + 8 ≤ n :=
+  Nat.le_induction
+  ---- Base case
+  ⟨7, Nat.add_le_add_left (Nat.succ_pos 27) 98, Nat.le_refl 99⟩
+  ---- Induction step
+  λ n h h0 ↦ by
+    rcases h0 with ⟨k, h0, h1⟩
+    rw [le_iff_lt_or_eq] at h0; rcases h0 with h0 | rfl
+    · exact ⟨k, h0, Nat.le_succ_of_le h1⟩
+    refine ⟨k + 1, ?_, ?_⟩
+    ---- `2k^2 + 4k + 1 < 2(k + 1)^2 + 4(k + 1)`
+    · rw [Nat.mul_succ, ← Nat.add_assoc]
+      exact Nat.add_le_add (Nat.add_le_add_right (Nat.mul_le_mul_left 2 <|
+        Nat.pow_le_pow_of_le_left k.le_succ 2) _) (Nat.le_add_left 1 3)
+    ---- `(k + 1)^2 + 6(k + 1) + 8 ≤ 2k^2 + 4k + 1`
+    · replace h1 : 2 * k ^ 2 + 4 * k + 2 = 2 * (k + 1) ^ 2 := by
+        rw [add_sq, Nat.mul_add, Nat.mul_add, Nat.mul_one, ← Nat.mul_assoc]; rfl
+      rw [← Nat.add_le_add_iff_right (n := 2), h1, Nat.succ_le_iff, Nat.mul_comm,
+        ← Nat.div_lt_iff_lt_mul (Nat.succ_pos 1), ← Nat.sqrt_lt'] at h
+      change 8 ≤ k + 1 at h -- This is a bit slow, but it seems unavoidable
+      rw [← Nat.add_le_add_iff_right (n := 1), h1, Nat.two_mul,
+        Nat.add_assoc, Nat.add_assoc, Nat.add_le_add_iff_left, sq]
+      apply (Nat.mul_le_mul_right _ h).trans'
+      rw [Nat.add_mul 6 2, Nat.add_le_add_iff_left, Nat.two_mul]
+      exact Nat.add_le_add h (Nat.le_add_left 1 k)
+
+
 
 /-- Final solution -/
-theorem final_solution (h : 99 ≤ n) : good n := by
-  suffices ∃ k, n ≤ 2 * k ^ 2 + 4 * k ∧ k ^ 2 + 6 * k + 8 ≤ n
-    from this.elim λ k h0 ↦ good_cond2 h0.1 h0.2
-  revert h; refine Nat.le_induction ?_ ?_ n
-  -- Base case
-  · exact ⟨7, Nat.add_le_add_left (Nat.succ_pos 27) 98, Nat.le_refl 99⟩
-  -- Induction step
-  · rintro n h ⟨k, h0, h1⟩
-    rw [le_iff_lt_or_eq] at h0
-    rcases h0 with h0 | rfl
-    · exact ⟨k, h0, h1.trans n.le_succ⟩
-    · refine ⟨k + 1, le_iff_exists_add.mpr ⟨4 * k + 5, by ring⟩, ?_⟩
-      replace h1 : (k + 1) ^ 2 + 6 * (k + 1) + 8
-        = 4 * k + 14 + (k ^ 2 + 4 * k + 1) := by ring
-      rw [two_mul, add_assoc (k ^ 2), add_assoc (k ^ 2), h1, add_le_add_iff_right]
-      -- Remaining goal: `4k + 14 ≤ k^2`
-      rw [← add_le_add_iff_right 2, mul_assoc 2 2 k, ← mul_add, ← mul_add_one] at h
-      replace h : 2 * 7 ^ 2 < 2 * (k ^ 2 + 2 * k + 1) :=
-        Nat.lt_of_succ_le <| h.trans' <| le_iff_exists_add.mpr ⟨2, by rfl⟩
-      rw [← one_pow 2, ← (2 * k).mul_one, ← add_sq] at h
-      replace h1 := lt_of_pow_lt_pow_left' 2 <|
-        lt_of_mul_lt_mul_left h (Nat.zero_le 2)
-      rw [Nat.lt_succ_iff] at h1
-      -- Now we obtained `k ≥ 7`; use it to finish
-      apply (add_le_add_left (Nat.mul_le_mul_left 2 h1) _).trans
-      rw [sq, ← add_mul]
-      exact Nat.mul_le_mul_right k (h1.trans' <| Nat.le_succ 6)
+theorem final_solution (h : 99 ≤ n) : good n :=
+  (good_cond2_k_witness n h).elim λ _ h ↦ good_cond2 h.1 h.2
