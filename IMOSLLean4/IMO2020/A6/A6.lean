f5c4fbaef7db8ac4e49ff5635ffe200d1189b916
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2024-08-06 07:59:14 -0400

    2020 A6: Reduce imports

--- IMOSLLean4/IMO2020/A6/A6.lean
+++ IMOSLLean4/IMO2020/A6/A6.lean
@@ -4,9 +4,8 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Gian Cordana Sanjaya
 -/
 
-import Mathlib.Algebra.Group.Int
-import Mathlib.Init.Data.Int.Order
-import Mathlib.Order.Basic
+import Mathlib.Data.Int.Defs
+import Mathlib.Logic.Function.Iterate
 import IMOSLLean4.Extra.NatSequence.SeqMax
 
 /-!
@@ -35,8 +34,10 @@ theorem add_one_iterate : ∀ n (a : ℤ), (· + 1)^[n] a = a + n := by
 
 theorem good_add_one : good (· + 1) := λ a b ↦ by
   have h (c : ℤ) : c + (c.natAbs ^ 2 : ℕ) = c * (c + 1) := by
-    rw [Int.natCast_pow, Int.natAbs_sq, sq, Int.mul_add, Int.mul_one, add_comm]
-  rw [add_one_iterate, Int.ofNat_add, add_add_add_comm, h, h]
+    rw [Int.natCast_pow, Int.natAbs_sq, Int.pow_succ, Int.pow_succ,
+      Int.pow_zero, c.one_mul, Int.mul_add, Int.mul_one, Int.add_comm]
+  rw [add_one_iterate, Int.ofNat_add, ← h, ← h,
+    a.add_assoc, b.add_left_comm, ← a.add_assoc]
 
 theorem const_iterate (a b : α) : ∀ n, (λ _ ↦ b)^[n] a = bif n.beq 0 then a else b
   | 0 => rfl
@@ -47,7 +48,7 @@ theorem good_zero : good (λ _ ↦ 0) := λ a b ↦ by
   cases h : (a.natAbs ^ 2 + b.natAbs ^ 2).beq 0 with | false => rfl | true => ?_
   have h0 (c : ℤ) : c.natAbs ^ 2 = 0 ↔ c = 0 := by
     rw [Nat.pow_eq_zero, and_iff_left (Nat.succ_ne_zero 1), Int.natAbs_eq_zero]
-  rw [Nat.beq_eq, add_eq_zero, h0, h0] at h
+  rw [Nat.beq_eq, Nat.add_eq_zero, h0, h0] at h
   rw [h.1, h.2]; rfl
 
 
@@ -65,13 +66,15 @@ theorem map_iterate_sq (a : ℤ) : f^[a.natAbs ^ 2] a = a * f a := by
 
 theorem map_neg_one : f (-1) = 0 := by
   have h : f (-1) = -1 * f (-1) := by exact map_iterate_sq h (-1)
-  rw [Int.neg_mul, Int.one_mul, eq_neg_iff_add_eq_zero, ← Int.two_mul, Int.mul_eq_zero] at h
+  rw [Int.neg_mul, Int.one_mul, ← Int.add_left_inj (k := f (-1)),
+    Int.add_right_neg, ← Int.two_mul, Int.mul_eq_zero] at h
   exact h.resolve_left (ne_of_beq_false rfl)
 
 theorem map_iterate_sq_add_one (a : ℤ) :
     f^[(a + 1).natAbs ^ 2 + 1] a = f^[(a + 1).natAbs ^ 2] (a + 1) := by
   have h0 := h (a + 1) (-1)
-  rwa [add_neg_cancel_right, map_neg_one h, Int.mul_zero, add_zero, ← map_iterate_sq h] at h0
+  rwa [Int.add_neg_cancel_right, map_neg_one h,
+    Int.mul_zero, Int.add_zero, ← map_iterate_sq h] at h0
 
 theorem eq_add_one_of_injective (h0 : f.Injective) : f = (· + 1) :=
   funext λ a ↦ h0.iterate ((a + 1).natAbs ^ 2) (map_iterate_sq_add_one h a)
@@ -82,20 +85,21 @@ theorem exists_iter_add_large_eq (a : ℤ) : ∀ k, ∃ N, f^[N + k] a = f^[N] (
   refine ⟨N + (a + k + 1).natAbs ^ 2, ?_⟩
   rw [f.iterate_add_apply N, Int.natCast_succ, Int.succ, ← a.add_assoc,
     ← map_iterate_sq_add_one h, Commute.iterate_iterate_self, ← h0,
-    ← iterate_add_apply, add_comm _ (N + k), add_add_add_comm]
+    ← iterate_add_apply, Nat.add_comm _ (N + k), Nat.add_add_add_comm]
 
 theorem orbit_zero_bdd_of_not_injective (h0 : ¬f.Injective) :
     ∃ M, ∀ n, (f^[n] 0).natAbs < M := by
   ---- First get `f(a + k) = f(a)` for some `k > 0`
   replace h0 : ∃ (a : ℤ) (k : ℕ), 0 < k ∧ f (a + k) = f a := by
     simp only [Injective, not_forall] at h0
-    rcases h0 with ⟨x, y, h0, h1⟩
-    wlog h2 : x < y
-    · exact this h y x h0.symm (Ne.symm h1) ((Ne.lt_or_lt h1).resolve_left h2)
-    apply Int.sub_pos_of_lt at h2
-    lift y - x to ℕ using h2.le with k hk
-    refine ⟨x, k, Int.natAbs_pos.mpr h2.ne.symm, ?_⟩
-    rw [hk, add_sub_cancel, h0]
+    obtain ⟨x, y, h0, h1⟩ : ∃ x y, x < y ∧ f x = f y := by
+      rcases h0 with ⟨x, y, h0, h1⟩
+      obtain h1 | h1 : x < y ∨ y < x := Int.lt_or_lt_of_ne h1
+      exacts [⟨x, y, h1, h0⟩, ⟨y, x, h1, h0.symm⟩]
+    replace h0 := Int.sub_pos_of_lt h0
+    lift y - x to ℕ using Int.le_of_lt h0 with k hk
+    refine ⟨x, k, Int.ofNat_pos.mp h0, ?_⟩
+    rw [hk, ← Int.add_sub_assoc, x.add_comm, y.add_sub_cancel, h1]
   ---- Upgrade to `f^k` having a fixed point of form `f^N(a)`
   replace h0 : ∃ a k N, 0 < k ∧ f^[N + k] a = f^[N] a := by
     rcases h0 with ⟨a, k, hk, h0⟩
@@ -107,10 +111,11 @@ theorem orbit_zero_bdd_of_not_injective (h0 : ¬f.Injective) :
   replace h0 : ∃ k N, 0 < k ∧ f^[N + k] 0 = f^[N] 0 := by
     rcases h0 with ⟨a, k, N, hk, h0⟩
     obtain ⟨K, M, h1⟩ : ∃ K M, f^[K] a = f^[M] 0 := by
-      obtain h1 | h1 : 0 ≤ -a ∨ 0 ≤ a := (le_total a 0).imp_left (Int.neg_nonneg_of_nonpos)
+      obtain h1 | h1 : 0 ≤ -a ∨ 0 ≤ a :=
+        (Int.le_total a 0).imp_left (Int.neg_nonneg_of_nonpos)
       · lift -a to ℕ using h1 with m hm
         rcases exists_iter_add_large_eq h a m with ⟨M, h2⟩
-        rw [hm, add_neg_self] at h2; exact ⟨M + m, M, h2⟩
+        rw [hm, Int.add_right_neg] at h2; exact ⟨M + m, M, h2⟩
       · lift a to ℕ using h1
         rcases exists_iter_add_large_eq h 0 a with ⟨M, h2⟩
         rw [Int.zero_add, eq_comm] at h2; exact ⟨M, M + a, h2⟩
@@ -127,24 +132,24 @@ theorem orbit_zero_bdd_of_not_injective (h0 : ¬f.Injective) :
     exact Extra.le_seqMax_of_le (λ n ↦ (f^[n] 0).natAbs) h1
   ---- The goal reduces to `f^n(0) = f^j(0)` for some `j < N + k`
   refine Nat.rec ⟨0, (N + k).zero_le, rfl⟩ λ n ⟨j, hj, h1⟩ ↦ ?_
-  obtain h2 | rfl : j < N + k ∨ j = N + k := hj.lt_or_eq
+  obtain h2 | rfl : j < N + k ∨ j = N + k := Nat.lt_or_eq_of_le hj
   · refine ⟨j.succ, h2, ?_⟩
     rw [f.iterate_succ_apply', h1, f.iterate_succ_apply']
   · refine ⟨N + 1, Nat.add_le_add_left hk _, ?_⟩
     rw [f.iterate_succ_apply', h1, h0, f.iterate_succ_apply']
 
-theorem eq_zero_of_not_injective (h0 : ∃ M, ∀ n, (f^[n] 0).natAbs < M) : f = 0 := by
+theorem eq_zero_of_not_injective (h0 : ∃ M, ∀ n, (f^[n] 0).natAbs < M) : f = λ _ ↦ 0 := by
   rcases h0 with ⟨M, h0⟩
   have h1 (a : ℤ) : f^[2 * a.natAbs ^ 2] 0 = a * (f a - f (-a)) := by
-    have h1 := h a (-a); rwa [a.natAbs_neg, add_neg_self, ← Nat.two_mul,
-      Int.neg_mul, ← sub_eq_add_neg, ← Int.mul_sub] at h1
+    have h1 := h a (-a); rwa [a.natAbs_neg, Int.add_right_neg,
+      ← Nat.two_mul, Int.neg_mul, ← Int.sub_eq_add_neg, ← Int.mul_sub] at h1
   replace h0 (n) (ha : M ≤ n) : f^[2 * n ^ 2] 0 = 0 := by
     refine Int.natAbs_eq_zero.mp <| Nat.eq_zero_of_dvd_of_lt
-      ⟨(f n - f (-n)).natAbs, ?_⟩ ((h0 _).trans_le ha)
+      ⟨(f n - f (-n)).natAbs, ?_⟩ (Nat.lt_of_lt_of_le (h0 _) ha)
     have h2 : f^[2 * n ^ 2] 0 = _ := h1 n
     rw [h2, Int.natAbs_mul, Int.natAbs_ofNat]
   replace h0 : f^[2] 0 = 0 := by
-    have h2 : M ≤ M ^ 2 + 1 := sq M ▸ Nat.le_succ_of_le M.le_mul_self
+    have h2 : M ≤ M ^ 2 + 1 := M.pow_two ▸ Nat.le_succ_of_le M.le_mul_self
     replace h2 := h0 (M ^ 2 + 1) h2
     rwa [Nat.pow_two, (M ^ 2).succ_mul, ← Nat.add_assoc, ← Nat.mul_succ,
       Nat.mul_add_one, Nat.add_comm, f.iterate_add_apply, ← Nat.mul_assoc,
@@ -153,15 +158,16 @@ theorem eq_zero_of_not_injective (h0 : ∃ M, ∀ n, (f^[n] 0).natAbs < M) : f =
     replace h1 : f a = f (-a) := by
       specialize h1 a
       rw [f.iterate_mul, Function.iterate_fixed h0, eq_comm, Int.mul_eq_zero] at h1
-      exact eq_of_sub_eq_zero (h1.resolve_left ha)
+      exact Int.eq_of_sub_eq_zero (h1.resolve_left ha)
     have h2 := h.map_iterate_sq (-a)
     obtain ⟨n, h3⟩ : ∃ n : ℕ, a.natAbs ^ 2 = n.succ := by
       apply Nat.exists_eq_succ_of_ne_zero
       rw [Ne, Nat.pow_eq_zero, not_and_or, Int.natAbs_eq_zero]
       left; exact ha
     rwa [Int.natAbs_neg, h3, f.iterate_succ_apply, ← h1, ← f.iterate_succ_apply, ← h3,
-      h.map_iterate_sq, Int.neg_mul, eq_neg_iff_add_eq_zero, ← Int.two_mul, Int.mul_eq_zero,
-      or_iff_right (ne_of_beq_false (by rfl)), Int.mul_eq_zero, or_iff_right ha] at h2
+      h.map_iterate_sq, Int.neg_mul, ← Int.add_left_inj (k := a * f a), Int.add_right_neg,
+      ← Int.two_mul, Int.mul_eq_zero, or_iff_right (ne_of_beq_false (by rfl)),
+      Int.mul_eq_zero, or_iff_right ha] at h2
   funext a; obtain ha | rfl : a ≠ 0 ∨ a = 0 := ne_or_eq a 0
   · exact h1 a ha
   · have h2 : f (f (-2)) = -1 * f (-1) + -1 * f (-1) := by exact h (-1) (-1)
