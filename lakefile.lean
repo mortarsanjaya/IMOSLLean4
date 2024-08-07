0929575a278ae86de4be9344b1f6df725299c57a
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2024-08-06 20:42:28 -0400

    Recover doc-gen4 capability

--- lakefile.lean
+++ lakefile.lean
@@ -14,3 +14,6 @@ require mathlib from git
 @[default_target]
 lean_lib «IMOSLLean4» where
   -- add any library configuration options here
+
+meta if get_config? env = some "dev" then -- dev is so not everyone has to build it
+require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
