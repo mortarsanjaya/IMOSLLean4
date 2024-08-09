f1059dc400adee39d13e663663a3f054576d645b
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2024-08-08 21:49:28 -0400

    2009 N1: Documentation fix

--- IMOSLLean4/IMO2009/N1/N1.lean
+++ IMOSLLean4/IMO2009/N1/N1.lean
@@ -10,7 +10,7 @@ import Mathlib.Data.ZMod.Defs
 # IMO 2009 N1 (P1)
 
 Let $n$ be a positive integer.
-Let $a_1, a_2, …, a_k$ be distinct integers in $\\{1, 2, …, n\\}$, with $k > 1$.
+Let $a_1, a_2, …, a_k$ be distinct integers in $\{1, 2, …, n\}$, with $k > 1$.
 Prove that there exists $i ≤ k$ such that $n$ does not divide $a_i (a_{i + 1} - 1)$.
 Here, we denote $a_{k + 1} = a_1$.
 -/
