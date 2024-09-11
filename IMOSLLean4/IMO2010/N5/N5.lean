b45cf18ab3733aaa388b73a3870277d605262033
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2024-09-11 03:37:58 -0400

    2010 N5: Reduce imports

--- IMOSLLean4/IMO2010/N5/N5.lean
+++ IMOSLLean4/IMO2010/N5/N5.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Gian Cordana Sanjaya
 -/
 
-import Mathlib.Data.Nat.GCD.Basic
 import Mathlib.Data.Nat.Prime.Basic
 import Mathlib.Data.Nat.ModEq
 
