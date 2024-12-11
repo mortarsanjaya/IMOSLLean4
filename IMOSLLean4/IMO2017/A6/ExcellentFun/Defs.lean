dd5973202158651557aa3ada807ffbe70939664c
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2024-12-10 19:07:41 -0500

    2017 A6: Basic lemmas

--- IMOSLLean4/IMO2017/A6/ExcellentFun/Defs.lean
+++ IMOSLLean4/IMO2017/A6/ExcellentFun/Defs.lean
@@ -28,9 +28,12 @@ class ExcellentFunClass (F R G) [NonAssocRing R] [Add G] [FunLike F R G] where
   excellent_def : ∀ (f : F) (x y : R), f (x + y - x * y) + f (1 - (x + y)) = f (1 - x * y)
 
 
+variable [NonAssocRing R]
+
+
 namespace ExcellentFun
 
-variable [NonAssocRing R] [Add G]
+variable [Add G]
 
 instance : FunLike (ExcellentFun R G) R G where
   coe f := f.toFun
@@ -47,7 +50,7 @@ end ExcellentFun
 
 section
 
-variable [NonAssocRing R] [Add G] [FunLike F R G] [ExcellentFunClass F R G] (f : F)
+variable [Add G] [FunLike F R G] [ExcellentFunClass F R G] (f : F)
 
 /-- Coercion from `ExcellentFunClass` to `ExcellentFun`. -/
 def ExcellentFunClass.toExcellentFun : ExcellentFun R G where
@@ -63,13 +66,83 @@ end
 
 
 
+/-! ### Some lemmas on excellent functions -/
+
+section
+
+variable [AddZeroClass G] [FunLike F R G] [ExcellentFunClass F R G] (f : F) (x : R)
+
+lemma excellent_map_self_add_map_one_sub : f x + f (1 - x) = f 1 := by
+  have h := excellent_def f 0 x
+  rwa [zero_add, zero_mul, sub_eq_add_neg,
+    sub_eq_add_neg 1 0, neg_zero, add_zero, add_zero] at h
+
+lemma excellent_map_one_add : f (1 + x) = f 1 + f x := by
+  have h := (excellent_def f 1 (-x)).symm
+  rwa [one_mul, sub_eq_add_neg, sub_eq_add_neg, sub_eq_add_neg, neg_add_rev,
+    neg_neg, neg_add_cancel_right, add_neg_cancel_comm_assoc] at h
+
+end
+
+
+lemma excellent_map_nat_add [AddMonoid G] [FunLike F R G] [ExcellentFunClass F R G]
+    (f : F) (x : R) (n : ℕ) : f (n + x) = n • f 1 + f x := by
+  induction n with
+  | zero => rw [Nat.cast_zero, zero_add, zero_nsmul, zero_add]
+  | succ n n_ih => rw [succ_nsmul', Nat.add_comm, Nat.cast_add,
+      Nat.cast_one, add_assoc, excellent_map_one_add, n_ih, add_assoc]
+
+lemma excellent_map_zero [AddZeroClass G] [IsCancelAdd G]
+    [FunLike F R G] [ExcellentFunClass F R G] (f : F) : f 0 = 0 :=
+  add_right_cancel (b := f (1 - 0)) <| calc
+    _ = f (0 + 0 - 0 * 0) + f (1 - (0 + 0)) := by
+      rw [add_zero, mul_zero, sub_eq_add_neg, sub_eq_add_neg, neg_zero, add_zero, add_zero]
+    _ = 0 + f (1 - 0) := by rw [excellent_def, zero_add, zero_mul]
+
+
+section
+
+variable [AddCancelMonoid G] [FunLike F R G] [ExcellentFunClass F R G] (f : F)
+
+lemma excellent_map_nat (n : ℕ) : f n = n • f 1 := by
+  rw [← add_zero (n : R), excellent_map_nat_add, excellent_map_zero, add_zero]
+
+lemma excellent_map_self_add_map_neg (x : R) : f x + f (-x) = 0 :=
+  add_left_cancel (a := f 1) <| calc
+    _ = f (1 + x) + f (-x) := by rw [excellent_map_one_add, add_assoc]
+    _ = f (1 + x) + f (1 - (1 + x)) := by
+      rw [sub_eq_add_neg, neg_add_rev, add_neg_cancel_comm_assoc]
+    _ = f 1 + 0 := by rw [excellent_map_self_add_map_one_sub, add_zero]
+
+lemma excellent_map_neg_add_map_self (x : R) : f (-x) + f x = 0 := by
+  rw [← excellent_map_self_add_map_neg f (-x), neg_neg]
+
+end
+
+
+section
+
+variable [AddCommMonoid G] [FunLike F R G] [ExcellentFunClass F R G] (f : F)
+
+lemma excellent_map_add_one (x : R) : f (x + 1) = f x + f 1 := by
+  rw [add_comm, excellent_map_one_add, add_comm]
+
+lemma excellent_map_add_nat (x : R) (n : ℕ) : f (x + n) = f x + n • f 1 := by
+  rw [add_comm, excellent_map_nat_add, add_comm]
+
+end
+
+
+
+
+
 /-! ### Some algebraic instances of excellent functions -/
 
 namespace ExcellentFun
 
 section
 
-variable [NonAssocRing R] [AddZeroClass G]
+variable [AddZeroClass G]
 
 /-- The zero function as a excellent function. -/
 protected def zero : ExcellentFun R G where
@@ -85,7 +158,7 @@ end
 
 section
 
-variable [NonAssocRing R] [AddCommSemigroup G]
+variable [AddCommSemigroup G]
 
 protected def add (f g : ExcellentFun R G) : ExcellentFun R G where
   toFun x := f x + g x
@@ -107,7 +180,7 @@ end
 
 section
 
-variable [NonAssocRing R] [AddCommMonoid G]
+variable [AddCommMonoid G]
 
 instance : AddCommMonoid (ExcellentFun R G) :=
   { ExcellentFun.instAddCommSemigroup with
@@ -123,36 +196,36 @@ instance : AddCommMonoid (ExcellentFun R G) :=
 end
 
 
-instance [NonAssocRing R] [AddCancelCommMonoid G] :
-    AddCancelCommMonoid (ExcellentFun R G) where
-  add_left_cancel _ _ _ h := ext λ x ↦ add_left_cancel (ExcellentFun.ext_iff.mp h x)
-
-
 section
 
-variable [NonAssocRing R] [SubtractionCommMonoid G]
+variable [AddCancelCommMonoid G]
 
 protected def neg (f : ExcellentFun R G) : ExcellentFun R G where
-  toFun x := -f x
-  excellent_def' x y := by rw [← neg_add_rev, add_comm, excellent_def]
+  toFun x := f (-x)
+  excellent_def' x y := add_right_cancel (b := f (1 - x * y)) <|
+    calc f (-(x + y - x * y)) + f (-(1 - (x + y))) + f (1 - x * y)
+    _ = f (-_) + f (-_) + (f (1 - (x + y)) + f (x + y - x * y)) := by
+      rw [← excellent_def, add_comm (f (1 - _))]
+    _ = f (-(x + y - x * y)) + ((f (-(1 - (x + y))) + f (1 - (x + y))) + f _) := by
+      rw [add_assoc, ← add_assoc (f (-(1 - (x + y))))]
+    _ = 0 := by rw [excellent_map_neg_add_map_self, zero_add, excellent_map_neg_add_map_self]
+    _ = f (-(1 - x * y)) + f (1 - x * y) := by rw [excellent_map_neg_add_map_self]
 
 instance : Neg (ExcellentFun R G) := ⟨ExcellentFun.neg⟩
 
-@[simp] theorem neg_apply (f : ExcellentFun R G) (x) : (-f) x = -f x := rfl
+@[simp] theorem neg_apply (f : ExcellentFun R G) (x) : (-f) x = f (-x) := rfl
 
 protected def sub (f g : ExcellentFun R G) : ExcellentFun R G := f + -g
 
 instance : Sub (ExcellentFun R G) := ⟨ExcellentFun.sub⟩
 
-@[simp] theorem sub_apply (f g : ExcellentFun R G) (x) : (f - g) x = f x - g x :=
-  (sub_eq_add_neg (f x) (g x)).symm
+@[simp] theorem sub_apply (f g : ExcellentFun R G) (x) : (f - g) x = f x + g (-x) := rfl
+
+instance : AddCommGroup (ExcellentFun R G) :=
+  { ExcellentFun.instAddCommMonoid with
+    neg_add_cancel := λ f ↦ ext (excellent_map_neg_add_map_self f)
+    zsmul := zsmulRec }
 
 end
-
-
-instance [NonAssocRing R] [AddCommGroup G] : AddCommGroup (ExcellentFun R G) :=
-  { ExcellentFun.instAddCommMonoid with
-    neg_add_cancel := λ f ↦ ext λ x ↦ neg_add_cancel (f x)
-    zsmul := zsmulRec }
 
 end ExcellentFun
