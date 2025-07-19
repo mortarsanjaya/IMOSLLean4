7e1722439730b90fc22809d319c7b3df0ef8bd78
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2025-07-19 06:07:07 -0400

    2006 A2: Move solution file

--- IMOSLLean4/IMO2006/IMO2006.lean
+++ IMOSLLean4/IMO2006/IMO2006.lean
@@ -1,6 +1,6 @@
 -- Root for the `IMO2006` problems.
 import IMOSLLean4.IMO2006.A1
-import IMOSLLean4.IMO2006.A2.A2
+import IMOSLLean4.IMO2006.A2
 import IMOSLLean4.IMO2006.A4.A4
 import IMOSLLean4.IMO2006.A6.A6
 import IMOSLLean4.IMO2006.N1.N1
