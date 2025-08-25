30861f5872ecb20e47226e76f54bfaac476bde95
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2025-08-25 01:46:52 -0400

    2024 N1: Formalize

--- IMOSLLean4/Main/IMO2024.lean
+++ IMOSLLean4/Main/IMO2024.lean
@@ -1,5 +1,6 @@
 -- Root for the `IMO2024` problems.
 import IMOSLLean4.Main.IMO2024.A1
 import IMOSLLean4.Main.IMO2024.A3
+import IMOSLLean4.Main.IMO2024.N1
 import IMOSLLean4.Main.IMO2024.N3
 import IMOSLLean4.Main.IMO2024.N4
