7fcfa38c8e35877fc9b19907b5bf0a4220d212c2
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2025-08-25 21:42:18 -0400

    2024 N6: Reorganize

--- IMOSLLean4/Main/IMO2024/N6.lean
+++ IMOSLLean4/Main/IMO2024/N6.lean
@@ -69,20 +69,26 @@ theorem good_of_good_polynomial_map [Ring R] [Ring S]
   dsimp only; rw [P.eval_map φ, P.eval₂_at_apply, ← φ.map_pow, ← φ.map_mul,
     ← φ.map_mul, ← φ.map_add, ← φ.map_add, ← φ.map_sub, ← φ.map_mul, hr, φ.map_zero]
 
+
+section
+
+variable [Ring R] [NoZeroDivisors R] {f : R → R}
+
 /-- If `R` is a domain, then `f : R → R` is good if and only if there exists `a, b, c ∈ R`
   such that `ar^2 + br + c ≠ 0` and `ar^2 + br + c ≠ f(r)` for any `r ∈ R`. -/
-theorem good_iff_of_domain [Ring R] [NoZeroDivisors R] {f : R → R} :
-    good f ↔ ∃ a b c : R, ∀ r : R,
-      a * r ^ 2 + b * r + c ≠ 0 ∧ a * r ^ 2 + b * r + c ≠ f r := by
+theorem good_iff_of_domain : good f ↔ ∃ a b c : R,
+    ∀ r : R, a * r ^ 2 + b * r + c ≠ 0 ∧ a * r ^ 2 + b * r + c ≠ f r := by
   conv => left; unfold good; right; ext; right; ext; right; ext; ext
           rw [mul_ne_zero_iff, sub_ne_zero, ne_comm (a := f _)]
 
-/-- If `f : R → R` does not attain some unit, then `f` is good. -/
-theorem good_of_map_ne_unit [Ring R]
-    (f : R → R) (u : Rˣ) (hf : ∀ r, f r ≠ u) : good f := by
-  refine ⟨0, 0, u, λ r ↦ ?_⟩
-  rw [zero_mul, zero_mul, zero_add, zero_add, ne_eq, Units.mul_right_eq_zero, sub_eq_zero]
-  exact hf r
+/-- If `R` is a domain and `f : R → R` does not attain some non-zero value,
+  then `f` is good. -/
+theorem good_of_map_ne_of_ne_zero (hu : u ≠ 0) (hf : ∀ r, f r ≠ u) : good f := by
+  refine good_iff_of_domain.mpr ⟨0, 0, u, λ r ↦ ?_⟩
+  rw [zero_mul, zero_mul, add_zero, zero_add]
+  exact ⟨hu, (hf r).symm⟩
+
+end
 
 
 section
@@ -155,7 +161,6 @@ theorem exists_ne_map_ne_of_four [Fintype S] [DecidableEq S] {f : S → S}
     rwa [mem_compl, mem_singleton, eq_comm]
   ---- Then there exists `y ≠ x` such that `f(z) ≠ y` for `z ≠ a, c`.
   obtain ⟨y, hy, hy0⟩ : ∃ y ≠ x, ∀ z ≠ c, z ≠ a → f z ≠ y := by
-    -- Thus `f` does not map `{c, a}ᶜ` into a superset of `{x}ᶜ`, and so we're done.
     replace h : ¬Set.SurjOn f ({c, a}ᶜ : Finset S) ({x}ᶜ : Finset S) :=
       λ h0 ↦ h.not_ge (card_le_card_of_surjOn f h0)
     rw [Set.SurjOn, Set.subset_def, not_forall] at h
@@ -168,64 +173,79 @@ theorem exists_ne_map_ne_of_four [Fintype S] [DecidableEq S] {f : S → S}
   ---- Now we claim that this `y` indeed works, i.e., `f(a), f(c) ≠ y`.
   refine ⟨y, hy, λ z ↦ ?_⟩
   clear h hy
-  ---- First consider the case `z = a`.
+  ---- Three cases to consider: `z = a`, `z = c`, and `z ≠ a, c`.
   obtain rfl | hza : z = a ∨ z ≠ a := dec_em _
   · exact hab0.trans_ne (hy0 b hbc hab.symm)
-  ---- Second, consider the case `z = c`.
   obtain rfl | hzc : z = c ∨ z ≠ c := dec_em _
   · exact hcd0.trans_ne (hy0 d hcd.symm had.symm)
-  ---- Finally, resolve the case `z ≠ a, c`.
   · exact hy0 z hzc hza
+
+/-- Let `R` be a domain. Suppose that every function `f : R → R` with `0 ∉ f(R)` is good.
+  Then every function `f : R → R` is good. -/
+theorem good_of_forall_map_ne_zero [Ring R] [IsDomain R] [DecidableEq R]
+    (hR : ∀ f : R → R, (∀ r, f r ≠ 0) → good f) (f : R → R) : good f := by
+  let g (r : R) : R := if f r = 0 then 1 else f r
+  /- Consider the map `g : R → R` with `g(r) = 1` for `f(r) = 0` and `g(r) = f(r)` else.
+    Then `g` takes only non-zero values, and so it is good. -/
+  obtain ⟨a, b, c, hg⟩ : ∃ a b c, ∀ r,
+      a * r ^ 2 + b * r + c ≠ 0 ∧ a * r ^ 2 + b * r + c ≠ g r := by
+    refine good_iff_of_domain.mp (hR _ λ r ↦ ?_)
+    dsimp only [g]; split_ifs with h
+    exacts [one_ne_zero, h]
+  ---- Now use `g` to show that `f` is good.
+  refine good_iff_of_domain.mpr ⟨a, b, c, λ r ↦ ?_⟩
+  set s : R := a * r ^ 2 + b * r + c
+  obtain ⟨hr, hr0⟩ : s ≠ 0 ∧ s ≠ g r := hg r
+  exact ⟨hr, λ (h2 : s = f r) ↦ hr0 (h2.trans (if_neg (h2.symm.trans_ne hr)).symm)⟩
+
+
+section
+
+variable [Field F] [Fintype F] [DecidableEq F]
+
+/-- Let `F` be a finite field, and fix two distinct elements `a, b ∈ F`.
+  Suppose that every function `f : F → F` with `f(a) = f(b)` and `0 ∉ f(R)` is good.
+  Then every function `f : F → F` is good. -/
+theorem good_of_forall_map_pair_eq_map_ne_zero
+    (hab : a ≠ b) (hF : ∀ f : F → F, f a = f b → (∀ r, f r ≠ 0) → good f) :
+    ∀ f : F → F, good f := by
+  ---- Reduce to the case where `f` does not attain `0`.
+  refine good_of_forall_map_ne_zero λ f hf ↦ ?_
+  /- Then `f` is not surjective, and so it is not injective either.
+    Let `c, d ∈ F` be two distinct elements such that `f(c) = f(d)`. -/
+  obtain ⟨c, d, h1, h2⟩ : ∃ c d, f c = f d ∧ c ≠ d := by
+    have h1 : ¬f.Surjective := λ h1 ↦ (h1 0).elim hf
+    replace h1 : ¬f.Injective := Finite.surjective_of_injective.mt h1
+    simpa only [Function.Injective, not_forall, exists_prop] using h1
+  ---- Now find `u ∈ Rˣ` and `v ∈ R` such that `uc + v = a` and `ud + v = b`.
+  obtain ⟨u, v, rfl, rfl⟩ : ∃ u : Fˣ, ∃ v : F, u * c + v = a ∧ u * d + v = b :=
+    linear_transform_solver hab h2
+  ---- Then `x ↦ f(u⁻¹(x - v))` being good implies that `f` is good.
+  refine (good_shift_scale_iff (u := u⁻¹) (v := u⁻¹ * -v)).mp (hF _ ?_ (λ _ ↦ hf _))
+  rwa [← mul_add, add_neg_cancel_right, Units.inv_mul_cancel_left,
+    ← mul_add, add_neg_cancel_right, Units.inv_mul_cancel_left]
 
 /-- Let `F` be a finite field of characteristic `≠ 2`.
   Then every function from `F` to itself is good. -/
-theorem good_of_char_ne_two [Field F] [Fintype F] [DecidableEq F] (hF : ringChar F ≠ 2) :
-    ∀ f : F → F, good f := by
-  have X : (1 : F) ≠ 0 := one_ne_zero
-  ---- Reduce to the case where `0 ∉ f(F)`.
-  intro f; wlog h : ∀ x, f x ≠ 0 generalizing f
-  · -- Consider `g : F → F` defined by `g(x) = 1` if `x = 0` and `g(x) = f(x)` otherwise.
-    obtain ⟨a, b, c, h⟩ : ∃ a b c : F, ∀ r : F, a * r ^ 2 + b * r + c ≠ 0 ∧
-        a * r ^ 2 + b * r + c ≠ if f r = 0 then 1 else f r := by
-      refine good_iff_of_domain.mp (this (λ x ↦ if f x = 0 then 1 else f x) ?_)
-      intro x; dsimp only
-      split_ifs with h
-      exacts [X, h]
-    -- Now that we've shown that `g` is good, use it to show that `f` is good.
-    refine good_iff_of_domain.mpr ⟨a, b, c, λ r ↦ ?_⟩
-    let s : F := a * r ^ 2 + b * r + c
-    obtain ⟨h0, h1⟩ : s ≠ 0 ∧ s ≠ if f r = 0 then 1 else f r := h r
-    refine ⟨h0, λ (h2 : s = f r) ↦ h1 ?_⟩
-    rw [h2, if_neg (h2.symm.trans_ne h0)]
-  ---- Now reduce to the case where `f(-1) = f(1)`.
-  wlog h0 : f 1 = f (-1) generalizing f
-  · -- Note that `f` is not injective. Find `c ≠ d ∈ F` such that `f(c) = f(d)`.
-    obtain ⟨c, d, h1, h2⟩ : ∃ c d, f c = f d ∧ c ≠ d := by
-      have h1 : ¬f.Surjective := λ h1 ↦ (h1 0).elim h
-      simpa [Function.Injective] using Finite.surjective_of_injective.mt h1
-    -- Now find `u ∈ Rˣ` and `v ∈ R` such that `uc + v = 1` and `ud + v = -1`.
-    obtain ⟨u, v, h3, h4⟩ : ∃ u : Fˣ, ∃ v : F, u * c + v = 1 ∧ u * d + v = -1 :=
-      linear_transform_solver (λ h3 ↦ h0 (congrArg f h3)) h2
-    -- Then `x ↦ f(u⁻¹(x - v))` being good implies that `f` is good.
-    refine (good_shift_scale_iff (u := u⁻¹) (v := u⁻¹ * -v)).mp (this _ (λ x ↦ h _) ?_)
-    rw [← mul_add, ← mul_add, ← h4, add_neg_cancel_right, Units.inv_mul_cancel_left,
-      ← h3, add_neg_cancel_right, Units.inv_mul_cancel_left, h1]
-  ---- If `f` does not attain a non-zero value, then we are done.
-  by_cases h1 : ∃ u : Fˣ, ∀ x, f x ≠ u
-  · rcases h1 with ⟨u, hu⟩
-    exact good_of_map_ne_unit f u hu
+theorem good_of_char_ne_two (hF : ringChar F ≠ 2) : ∀ f : F → F, good f := by
+  ---- Reduce to the case where `f(-1) = f(1)` and `f` does not attain zero.
+  refine good_of_forall_map_pair_eq_map_ne_zero (a := -1) (b := 1)
+    (Ring.neg_one_ne_one_of_char_ne_two hF) λ f hf hf0 ↦ ?_
+  ---- If `f` does not attain some non-zero value, then we are done.
+  by_cases h : ∃ u ≠ 0, ∀ x, f x ≠ u
+  · rcases h with ⟨u, hu, hu0⟩
+    exact good_of_map_ne_of_ne_zero hu hu0
   ---- Now suppose that `f` attains every non-zero value.
-  replace h1 : ∀ u ≠ 0, ∃ x, f x = u := by
-    simp only [ne_eq, not_exists, not_forall, not_not] at h1
-    exact λ u hu ↦ h1 (Units.mk0 u hu)
+  replace h : ∀ u ≠ 0, ∃ x, f x = u := by
+    simpa only [ne_eq, not_exists, not_and, not_forall, Decidable.not_not] using h
   ---- Let `r` be a non-square in `F`.
   obtain ⟨r', hr⟩ : ∃ r' : F, ¬IsSquare r' := FiniteField.exists_nonsquare hF
   ---- Choose `y` such that `f(y) = (1 - r') f(0)`, and note that `y ≠ 0`.
   obtain ⟨y, hy⟩ : ∃ y, f y = (1 - r') * f 0 :=
-    h1 _ (mul_ne_zero (sub_ne_zero.mpr λ hr0 ↦ hr (hr0 ▸ IsSquare.one)) (h 0))
+    h _ (mul_ne_zero (sub_ne_zero.mpr λ hr0 ↦ hr (hr0 ▸ IsSquare.one)) (hf0 0))
   have hr' : r' ≠ 0 := λ hr' ↦ hr (hr' ▸ IsSquare.zero)
   have hy0 : y ≠ 0 := by
-    rintro rfl; rw [eq_comm, mul_eq_right₀ (h 0), sub_eq_self] at hy
+    rintro rfl; rw [eq_comm, mul_eq_right₀ (hf0 0), sub_eq_self] at hy
     exact hr' hy
   ---- Let `r = y^2/r'`, and note that `r` is not a square either.
   let r : F := y ^ 2 / r'
@@ -236,10 +256,11 @@ theorem good_of_char_ne_two [Field F] [Fintype F] [DecidableEq F] (hF : ringChar
     λ hr0 ↦ hr ⟨x, by rw [← sq, eq_comm, ← sub_eq_zero, hr0]⟩
   ---- The function `φ(x) = f(x)/(x^2 - r)` does not attain a non-zero value.
   obtain ⟨a, ha, ha0⟩ : ∃ a ≠ 0, ∀ x, f x / (x ^ 2 - r) ≠ a := by
+    have X : (1 : F) ≠ 0 := one_ne_zero
     refine exists_ne_map_ne_of_four (a := -1) (b := 1) (c := 0) (d := y)
       (Ring.neg_one_ne_one_of_char_ne_two hF) (neg_ne_zero.mpr X) X hy0.symm ?_ ?_ _
     -- Verify that `φ(-1) = φ(1)`.
-    · rw [h0, neg_sq]
+    · rw [hf, neg_sq]
     -- Verify that `φ(0) = φ(y)`.
     · rw [hy, div_eq_div_iff (hr _) (hr _), zero_pow Nat.two_pos.ne.symm, zero_sub,
         mul_right_comm, mul_neg, one_sub_mul, neg_sub, mul_div_cancel₀ _ hr', mul_comm]
