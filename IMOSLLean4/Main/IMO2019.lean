3c4e109313f60d8106bf69cba1cdbac3805a5608
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2026-01-14 16:03:57 -0500

    2019N2: Formalize

--- IMOSLLean4/Main/IMO2019.lean
+++ IMOSLLean4/Main/IMO2019.lean
@@ -2,5 +2,6 @@
 import IMOSLLean4.Main.IMO2019.A1
 import IMOSLLean4.Main.IMO2019.A2.A2
 import IMOSLLean4.Main.IMO2019.C2.C2
+import IMOSLLean4.Main.IMO2019.N2
 import IMOSLLean4.Main.IMO2019.N3.N3
 import IMOSLLean4.Main.IMO2019.N4.N4
