bf3fbc9b94033296b9b2b1b8f44b9b3da49eb7bf
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2026-01-27 11:46:57 -0500

    2010A1: Revamp

--- IMOSLLean4/Main/IMO2010.lean
+++ IMOSLLean4/Main/IMO2010.lean
@@ -1,5 +1,5 @@
 -- Root for the `IMO2010` problems.
-import IMOSLLean4.Main.IMO2010.A1.A1
+import IMOSLLean4.Main.IMO2010.A1
 import IMOSLLean4.Main.IMO2010.A2
 import IMOSLLean4.Main.IMO2010.A3.A3
 import IMOSLLean4.Main.IMO2010.A4.A4
