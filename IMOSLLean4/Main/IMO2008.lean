0329d2e5913c323cc58e06dc1d0e64b511784a86
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2026-02-04 17:52:03 -0500

    2008N1: Formalize

--- IMOSLLean4/Main/IMO2008.lean
+++ IMOSLLean4/Main/IMO2008.lean
@@ -7,4 +7,5 @@ import IMOSLLean4.Main.IMO2008.A7.A7
 import IMOSLLean4.Main.IMO2008.C2
 import IMOSLLean4.Main.IMO2008.C3
 import IMOSLLean4.Main.IMO2008.C4.C4
+import IMOSLLean4.Main.IMO2008.N1
 import IMOSLLean4.Main.IMO2008.N6
