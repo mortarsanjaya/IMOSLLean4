d01f3c02acfb16fe4fc9b0be3c83716c9c7104f0
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2026-04-15 22:59:47 -0400

    2010A1: Reduce imports

--- IMOSLLean4/Main/IMO2010/A1.lean
+++ IMOSLLean4/Main/IMO2010/A1.lean
@@ -5,7 +5,6 @@ Authors: Gian Cordana Sanjaya
 -/
 
 import Mathlib.Algebra.Order.Floor.Ring
-import Mathlib.Algebra.Module.Torsion.Free
 
 /-!
 # IMO 2010 A1 (P1)
@@ -38,13 +37,13 @@ Finally, since $⌊-α⌋ = -1$, plugging $x = -α$ gives $f ≡ 0$.
 ### Extra
 
 It can be shown that there exists $α ∈ R$ such that $0 < α < 1$
-  as long as $R$ is not isomorphic to either $ℤ$ or the trivial ring.
+  as long as $R$ is not isomorphic to $ℤ$.
 -/
 
 namespace IMOSL
 namespace IMO2010A1
 
-variable  [Ring R] [LinearOrder R] [FloorRing R] [Ring S] [LinearOrder S] [FloorRing S]
+variable [Ring R] [LinearOrder R] [FloorRing R] [Ring S] [LinearOrder S] [FloorRing S]
 
 /-- A function `f : R → S` is called *good* if
   `f(⌊x⌋ y) = ⌊f(y)⌋ f(x)` for all `x, y : R`. -/
@@ -61,12 +60,14 @@ theorem const_is_good_of_floor_eq_one {C : S} (hC : ⌊C⌋ = 1) : good (λ _ : 
 
 namespace good
 
+open IsAddTorsionFree
+
 /-- We have either `⌊f(0)⌋ = 1` or `f(0) = 0`. -/
-theorem floor_eq_one_or_eq_zero [IsOrderedAddMonoid S] {f : R → S} (hf : good f) :
-    ⌊f 0⌋ = 1 ∨ f 0 = 0 := by
+theorem map_zero_eq_zero_or_floor_eq_one [IsOrderedAddMonoid S] {f : R → S} (hf : good f) :
+    f 0 = 0 ∨ ⌊f 0⌋ = 1 := by
   have h : f (⌊(0 : R)⌋ • 0) = ⌊f 0⌋ • f 0 := hf 0 0
   rwa [zsmul_zero, eq_comm, ← add_neg_eq_zero,
-    ← sub_one_zsmul, smul_eq_zero, sub_eq_zero] at h
+    ← sub_one_zsmul, zsmul_eq_zero_iff, sub_eq_zero] at h
 
 /-- If `⌊f(0)⌋ = 1`, then `f` is constant. -/
 theorem eq_const_of_floor_map_zero_eq_one {f : R → S} (hf : good f) (hf0 : ⌊f 0⌋ = 1) :
@@ -92,8 +93,8 @@ theorem eq_zero_of_map_zero_eq_zero
   ---- First plug `x = y = α` and get `⌊f(α)⌋ = 0`.
   replace h : ⌊f α⌋ = 0 := by
     have h : f (⌊α⌋ • α) = ⌊f α⌋ • f α := hf α α
-    rw [hα, zero_zsmul, hf0, eq_comm, smul_eq_zero] at h
-    exact h.elim id λ h0 ↦ (congrArg Int.floor h0).trans Int.floor_zero
+    rw [hα, zero_zsmul, hf0, eq_comm, zsmul_eq_zero_iff] at h
+    exact h.elim (λ h0 ↦ (congrArg Int.floor h0).trans Int.floor_zero) id
   ---- Now plug `(x, y) = (-1, α)` and get `f(-α) = 0`.
   replace h : f (-α) = 0 := by
     have h0 : f (⌊(-1 : R)⌋ • α) = ⌊f α⌋ • f (-1) := hf (-1) α
@@ -109,9 +110,9 @@ end good
 /-- Final solution -/
 theorem final_solution [IsStrictOrderedRing R] (hR : ∃ α : R, 0 < α ∧ α < 1)
     [IsStrictOrderedRing S] {f : R → S} :
-    good f ↔ (∃ C : S, ⌊C⌋ = 1 ∧ f = λ _ ↦ C) ∨ f = 0 := by
+    good f ↔ f = 0 ∨ (∃ C : S, ⌊C⌋ = 1 ∧ f = λ _ ↦ C) := by
   refine ⟨λ hf ↦ ?_, λ hf ↦ ?_⟩
-  · exact hf.floor_eq_one_or_eq_zero.imp
-      hf.eq_const_of_floor_map_zero_eq_one (hf.eq_zero_of_map_zero_eq_zero hR)
-  · rcases hf with ⟨C, hC, rfl⟩ | rfl
-    exacts [const_is_good_of_floor_eq_one hC, zero_is_good]
+  · exact hf.map_zero_eq_zero_or_floor_eq_one.imp
+      (hf.eq_zero_of_map_zero_eq_zero hR) hf.eq_const_of_floor_map_zero_eq_one
+  · rcases hf with rfl | ⟨C, hC, rfl⟩
+    exacts [zero_is_good, const_is_good_of_floor_eq_one hC]
